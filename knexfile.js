5030b9e63236c21a658bc6073e5391980e8d888b
Andrew Eskenazi <drewesk@gmail.com>
2017-06-17 15:34:20 -0600

    Bug fixes with Knexfile.js.

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,13 @@
 module.exports = {
+
   development: {
     client: 'pg',
     connection: 'postgres://localhost/crud-reviews'
+  },
+
+  production: {
+  client: 'postgresql',
+    connection: process.env.DATABASE_URL
   }
+
 };
